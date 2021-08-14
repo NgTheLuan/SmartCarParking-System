8a609a6318a35064582de636f29a271155e8049e
NgTheLuan <nguyenluan.work@gmail.com>
2021-08-14 20:30:32 +0700

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDZcQWZClnfZ7vxYmtemOy5MAx_BW2vuJU',
-    authDomain: 'test-bdf44.firebaseapp.com',
-    databaseURL: 'https://test-bdf44-default-rtdb.firebaseio.com',
-    projectId: 'test-bdf44',
-    storageBucket: 'test-bdf44.appspot.com',
-    messagingSenderId: '1073804088014',
-    appId: '1:1073804088014:web:25f217b7a30cdf44a3b69f',
-    measurementId: 'G-CRW2K1GLNF',
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
   },
 };
